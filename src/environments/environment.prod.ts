0697aa59d5d56f41cc169cfadc74bbbb6f3b207a
aletheon <naitaubafarm@gmail.com>
2020-10-12 21:12:22 +1300

    changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB8jm4QghgWtQfcTd9S4pkG8sUUrI97FYE",
+    authDomain: "eleutherios-website.firebaseapp.com",
+    databaseURL: "https://eleutherios-website.firebaseio.com",
+    projectId: "eleutherios-website",
+    storageBucket: "eleutherios-website.appspot.com",
+    messagingSenderId: "825423870522",
+    appId: "1:825423870522:web:0171b6dcbecd589792d303",
+    measurementId: "G-MWBCJKXMZC"
+  },
+  googleTagManagerId: "GTM-TSTXZ64"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB8jm4QghgWtQfcTd9S4pkG8sUUrI97FYE",
+    authDomain: "eleutherios-website.firebaseapp.com",
+    databaseURL: "https://eleutherios-website.firebaseio.com",
+    projectId: "eleutherios-website",
+    storageBucket: "eleutherios-website.appspot.com",
+    messagingSenderId: "825423870522",
+    appId: "1:825423870522:web:0171b6dcbecd589792d303",
+    measurementId: "G-MWBCJKXMZC"
+  },
+  googleTagManagerId: "GTM-TSTXZ64"
 };
 
 /*
