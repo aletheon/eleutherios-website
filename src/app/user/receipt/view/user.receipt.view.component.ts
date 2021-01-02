0a06a10332989cb4fdb2076a8719a2c15cd509be
aletheon <naitaubafarm@gmail.com>
2021-01-02 17:25:39 +1300

    changed

--- src/app/user/receipt/view/user.receipt.view.component.ts
+++ src/app/user/receipt/view/user.receipt.view.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { AuthService } from '../../../core/auth.service';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import {
-  UserReceiptService
+  SiteTotalService,
+  UserReceiptService,
+  UserServiceImageService,
+  UserServiceTagService,
+  NoTitlePipe,
+  TruncatePipe
 } from '../../../shared';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { NotificationSnackBar } from '../../../shared/components/notification.snackbar.component';
 
-import { Observable, Subscription, BehaviorSubject } from 'rxjs';
+import { Observable, Subscription, BehaviorSubject, of, zip, combineLatest, from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import * as firebase from 'firebase/app';
+import * as _ from "lodash";
+
 
 @Component({
   selector: 'user-receipt-view',
@@ -23,17 +31,24 @@ export class UserReceiptViewComponent implements OnInit, OnDestroy {
   private _loading = new BehaviorSubject(false);
   private _initialReceiptSubscription: Subscription;
   private _receiptSubscription: Subscription;
+  private _defaultServiceImageSubscription: Subscription;
+  private _serviceTagSubscription: Subscription;
   
   public receiptGroup: FormGroup;
   public receipt: Observable<any>;
+  public defaultServiceImage: Observable<any>;
+  public serviceTags: Observable<any[]>;
   public loading: Observable<boolean> = this._loading.asObservable();
 
   constructor(public auth: AuthService,
     private route: ActivatedRoute,
     private userReceiptService: UserReceiptService,
+    private userServiceImageService: UserServiceImageService,
+    private userServiceTagService: UserServiceTagService,
     private fb: FormBuilder, 
     private router: Router,
-    private snackbar: MatSnackBar) {
+    private snackbar: MatSnackBar,
+    private changeDetector : ChangeDetectorRef) {
   }
 
   ngOnDestroy () {
@@ -42,6 +57,12 @@ export class UserReceiptViewComponent implements OnInit, OnDestroy {
 
     if (this._receiptSubscription)
       this._receiptSubscription.unsubscribe();
+
+    if (this._defaultServiceImageSubscription)
+      this._defaultServiceImageSubscription.unsubscribe();
+
+    if (this._serviceTagSubscription)
+      this._serviceTagSubscription.unsubscribe();
   }
 
   ngOnInit () {
@@ -98,6 +119,64 @@ export class UserReceiptViewComponent implements OnInit, OnDestroy {
         this.receiptGroup.patchValue(receipt);
       }
       this._loading.next(false);
+    });
+
+    // run once subscription
+    const runOnceSubscription = this.receipt.subscribe(receipt => {
+      if (receipt){
+        let load = async function(){
+          try {
+            that._defaultServiceImageSubscription = that.userServiceImageService.getDefaultServiceImages(receipt.sellerUid, receipt.sellerServiceId).pipe(
+              switchMap(serviceImages => {
+                if (serviceImages && serviceImages.length > 0){
+                  let getDownloadUrl$: Observable<any>;
+
+                  if (serviceImages[0].smallUrl)
+                    getDownloadUrl$ = from(firebase.storage().ref(serviceImages[0].smallUrl).getDownloadURL());
+
+                  return combineLatest([getDownloadUrl$]).pipe(
+                    switchMap(results => {
+                      const [downloadUrl] = results;
+                      
+                      if (downloadUrl)
+                        serviceImages[0].url = downloadUrl;
+                      else
+                        serviceImages[0].url = '../../../assets/defaultThumbnail.jpg';
+        
+                      return of(serviceImages[0]);
+                    })
+                  );
+                }
+                else return of(null);
+              })
+            )
+            .subscribe(serviceImage => {
+              that.defaultServiceImage = of(serviceImage);
+            });
+
+            that._serviceTagSubscription = that.userServiceTagService.getTags(receipt.sellerUid, receipt.sellerServiceId).subscribe(serviceTags => {
+              if (serviceTags && serviceTags.length > 0)
+                that.serviceTags = of(serviceTags);
+              else
+                that.serviceTags = of([]);
+            });
+          }
+          catch (error) {
+            console.log('here');
+            throw error;
+          }
+        }
+    
+        // call load
+        load().then(() => {
+          this._loading.next(false);
+          this.changeDetector.detectChanges();
+          runOnceSubscription.unsubscribe();
+        })
+        .catch((error) =>{
+          console.log('initForm ' + error);
+        });
+      }
     });
   }
 }
