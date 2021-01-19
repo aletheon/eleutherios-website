eab2f78264e19ae9d5571a210a412ad7a0b61203
Rob <naitaubafarm@gmail.com>
2021-01-19 23:51:58 +1300

    changed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -75,6 +75,8 @@ export class LoginComponent implements OnInit {
           const userRef: AngularFirestoreDocument<User> = that.afs.doc(`users/${authResult.user.uid}`);
           userRef.ref.get().then((doc) => {
             if (doc.exists) {
+              console.log('user exists updating user');
+
               // const data: any = {
               //   email: authResult.user.email,
               //   displayName: authResult.user.displayName,
@@ -83,6 +85,8 @@ export class LoginComponent implements OnInit {
               // };
               // userRef.update(data);
             } else {
+              console.log('doesnt exist creating user');
+
               const data: User = {
                 uid: authResult.user.uid,
                 email: authResult.user.email,
