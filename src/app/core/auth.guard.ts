6ae49882a7e132694d08b9ecdf94bba9f945157e
Rob <naitaubafarm@gmail.com>
2021-01-22 10:57:49 +1300

    changed

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, UrlTree } from '@angular/router';
 import { AuthService } from './auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 import { Observable } from 'rxjs';
 import { map, take, tap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService, private router: Router){}
+  constructor(private auth: AuthService, private afAuth: AngularFireAuth, private router: Router){}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      return this.auth.user.pipe(
-        take(1),
+      return this.afAuth.user.pipe(
         map(user => !!user),
+        take(1),
         tap(loggedIn => { 
           if (!loggedIn)
             this.router.navigate(['/anonymous/home']);
