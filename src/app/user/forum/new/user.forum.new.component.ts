c9ac2cc97f83270a283c71d6db943d1f813ccf70
Rob <naitaubafarm@gmail.com>
2021-03-30 21:00:14 +1300

    changed

--- src/app/user/forum/new/user.forum.new.component.ts
+++ src/app/user/forum/new/user.forum.new.component.ts
@@ -209,8 +209,12 @@ export class UserForumNewComponent implements OnInit, OnDestroy, AfterViewInit  
     else {
       this.userForumImageService.exists(this.forumGroup.get('uid').value, this.forumGroup.get('forumId').value, image.imageId).then(exists => {
         if (!exists){
-          image.default = false;
-          this.userForumImageService.create(this.forumGroup.get('uid').value, this.forumGroup.get('forumId').value, image);
+          this.userImageService.getImage(image.uid, image.imageId).pipe(take(1)).subscribe(fetchedImage => {
+            if (fetchedImage){
+              fetchedImage.default = false;
+              this.userForumImageService.create(this.forumGroup.get('uid').value, this.forumGroup.get('forumId').value, fetchedImage);
+            }
+          });
         }
         else console.log('image already exists in forum');
       });
